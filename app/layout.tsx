e4fb28122723a598ca2170739a7dc439693dac32
Nargis Fatima <nargis@modfxmedia.com>
2026-09-16 18:49:34 +0500

    MS clarity code

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj6mgwtmq6");
+          `}
+        </Script>
         <SiteHeader />
         {children}
         <SiteFooter />
